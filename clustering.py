6177854564e1929e6a9d23f343f6c02adafcfde8
Yiming992 <yimingliuno1@gmail.com>
2018-12-20 17:54:16 +0800

    implement clustering algorithm used in inference

--- clustering.py
+++ clustering.py
@@ -1,17 +1,48 @@
 ####Inference阶段的聚类算法
-
-from collections import defaultdict
 import numpy as np 
 from sklearn.cluster import MeanShift,estimate_bandwidth
-import time
-import cv2
-import torch
-
 class lane_cluster():
 
-	def __init__(self):
+	def __init__(self,bandwidth,embedding,binary_mask):
+		self.color=[np.array([255,0,0]),
+		            np.array([0,255,0]),
+					np.array([0,0,255]),
+					np.array([125,125,0]),
+					np.array([0,125,125]),
+					np.array([125,0,125]),
+					np.array([50,100,50]),
+					np.array([100,50,100])]
+                    
+        self.bandwidth=bandwidth
+        self.embedding=embedding
+        self.binary=binary_mask
 
-		self.color=
+    def _get_lane_area(self):        
+        idx=np.where(self.binary==1)
+        lane_area=[]
+        lane_idx=[]
+        for i,j in zip(idx):
+            lane_area.append(self.instance[:,i,j])
+            lane_idx.append((i,j))
+        return np.array(lane_area),lane_idx
+
+    def _cluster(self,prediction):
+        ms=MeanShift(bandwidth=self.bandwidth,bin_seeding=True)
+        clustering=ms(prediction)
+        return clustering.cluster_centers_,clustering.labels
+
+    def _get_instance_masks(self,prediction):
+        lane_area,lane_idx=self._get_lane_area()
+        instance_mask=np.zeros(self.binary.shape[0],self.binary.shape[1],3)
+
+        centers,labels=self._cluster(lane_area)
+        for index,label in enumerate(labels):
+            instance_mask[lane_idx[index][0],lane_idx[index][1],:]=self.color[label]
+        return instance_mask
+   
+    def __call__(self):
+        return self._get_instance_masks()
+        
 
 
 
