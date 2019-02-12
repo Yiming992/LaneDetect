7f25780e7a4b21d8f65f7586ebaefac3d05e0796
Yiming992 <yimingliuno1@gmail.com>
2019-02-12 17:57:20 +0800

    complete make_predictions.py

--- clustering.py
+++ clustering.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.cluster import MeanShift,estimate_bandwidth
 class lane_cluster():
 
-    def __init__(self,bandwidth,embedding,binary_mask):
+    def __init__(self,bandwidth,image,embedding,binary_mask):
         self.color=[np.array([255,0,0]),
                     np.array([0,255,0]),
                     np.array([0,0,255]),
@@ -12,7 +12,8 @@ class lane_cluster():
                     np.array([125,0,125]),
                     np.array([50,100,50]),
                     np.array([100,50,100])]
-                    
+        
+        self.image=image
         self.bandwidth=bandwidth
         self.embedding=embedding
         self.binary=binary_mask
@@ -21,24 +22,24 @@ class lane_cluster():
         idx=np.where(self.binary==1)
         lane_area=[]
         lane_idx=[]
-        for i,j in zip(idx):
-            lane_area.append(self.instance[:,i,j])
+        for i,j in zip(*idx):
+            lane_area.append(self.embedding[:,i,j])
             lane_idx.append((i,j))
         return np.array(lane_area),lane_idx
 
     def _cluster(self,prediction):
-        ms=MeanShift(bandwidth=self.bandwidth,bin_seeding=True)
-        clustering=ms(prediction)
-        return clustering.cluster_centers_,clustering.labels
+        clustering=MeanShift(bandwidth=self.bandwidth,bin_seeding=True).fit(prediction)
+        return clustering.cluster_centers_,clustering.labels_
 
-    def _get_instance_masks(self,prediction):
+    def _get_instance_masks(self):
         lane_area,lane_idx=self._get_lane_area()
-        instance_mask=np.zeros(self.binary.shape[0],self.binary.shape[1],3)
+        print(self.binary.shape)
+        instance_mask=self.image
 
         centers,labels=self._cluster(lane_area)
         for index,label in enumerate(labels):
             instance_mask[lane_idx[index][0],lane_idx[index][1],:]=self.color[label]
-        return instance_mask
+        return instance_mask,labels
    
     def __call__(self):
         return self._get_instance_masks()
--- make_predictions.py
+++ make_predictions.py
@@ -4,7 +4,36 @@ import torch
 import numpy as np
 import cv2
 import argparse
+from torch.nn import DataParallel
+import os
 ### To be completed
+def forward(model,img_path):
+    img=cv2.imread(img_path,cv2.IMREAD_UNCHANGED)
+    img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    img=cv2.resize(img,(256,512))
+    image=img
+    img=np.transpose(img,axes=[2,0,1])
+    img=torch.tensor(img,dtype=torch.float)
+    img=img.unsqueeze(dim=0)
+    img=img.cuda()  
+    segmentation,embedding=model(img)
+    return image,binary_mask,embedding
+
+def gen_binary(segmentation,threshold=.5):
+    segmentation=segmentation.data.cpu().numpy()
+    binary_mask=segmentation.squeeze()
+    binary_mask=np.exp(binary_mask)
+    binary_mask=binary_mask/binary_mask.sum(axis=0)
+    if threshold:
+        binary_mask=binary_mask[1,:,:]>threshold
+        binary_mask=binary_mask.astype(np.float)
+    return binary_mask
+    
+def gen_instance(image,binary_mask,embedding):
+    clustering=lane_cluster(None,image,binary_mask,embedding)
+    instance_mask,labels=clustering()
+    return instance_mask
+
 if __name__=='__main__':
 
     ap=argparse.ArgumentParser()
@@ -13,26 +42,37 @@ if __name__=='__main__':
     ap.add_argument('-a','--address',required=True)
     ap.add_argument('-d','--device',default='GPU')
     ap.add_argument('-s','--save',default='./logs/predictions')
+    ap.add_argument('-p','--parallel',default='Yes')
+    ap.add_argument('-w','--weights',required=True)
 
     args=vars(ap.parse_args())
 
-    if args['device']=='GPU':
-        device=torch.device('cuda',0)
-    else:
-        device=torch.device('cpu')
-    
     model=LaneNet()
-    model=model.to(device)
-    model.eval()
+
+    if args['device']=='GPU':
+        if args['parallel']=='Yes':
+            model=DataParallel(model)
+        model.load_state_dict(args['weights'])
+        model.cuda()
+        model.eval()
+    else:
+        model.load_state_dict(args['weights'])
+        model.eval()
+    
     if args['mode']=='single':
-        img=cv2.imread(args['address'],cv2.IMREAD_UNCHANGED)
-        img=cv2.resize(img,(256,512))
-        img=img/255
-        img=torch.tensor(img)
-        img=img.to(device)
-        prediction=model(img)
-        prediction=prediction.data.cpu().numpy()
-        cv2.imwrite(args['save'])
+        image,segmentation,embedding=forward(model,args['address'])
+        binary_mask=gen_binary(segmentation)
+        instance_mask=gen_instance(image,binary_mask,embedding)
+
+        cv2.imwrite(args['save'],instance_mask)
     elif args['mode']=='batch':
         img_files=os.listdir(args['address'])
+        for i in img_files:
+            image,segmentation,embedding=forward(model,args['address'])
+            binary_mask=gen_binary(segmentation)
+            instance_mask=gen_instance(image,binary_mask,embedding)
+            cv2.imwrite(os.path.join(args['save'],i),instance_mask)
+
+
+
 
