464f79a8b5dd241f44b66691eb6ede1966b15985
Yiming992 <yimingliuno1@gmail.com>
2019-02-12 10:52:23 +0800

    updates

--- train.py
+++ train.py
@@ -53,6 +53,7 @@ def train(model,data,epoch,batch,delta_v,
     optimizer=torch.optim.Adam(params,lr=lr)
     start_time=int(time.time())
     log=open('./logs/loggings/LaneNet_{}.txt'.format(start_time),'w')
+    step=0
     for e_p in range(epoch):
         for batch_id,batch_data in enumerate(data['train']):
             input_data=batch_data[0]
@@ -67,31 +68,33 @@ def train(model,data,epoch,batch,delta_v,
             predictions,embeddings=model(input_data)
             total_loss,Variance,Distance,Reg=compute_loss(predictions,embeddings,seg_mask,instance_mask,
                                     class_weight,delta_v,delta_d)                              
-            log.write('Steps:{}, Loss:{}\n'.format(batch_id*(e_p+1),total_loss))
+            log.write('Steps:{}, Loss:{}\n'.format(step,total_loss))
             log.flush()
             #print('v:{},d:{},r:{}'.format(Variance,Distance,Reg))
             optimizer.zero_grad()
             total_loss.backward()
             clip_grad_value_(model.parameters(),clip_value=5.)
             optimizer.step()
+            step+=1
             e=time.time()
             print('step time:{}'.format(e-s))
             #print(list(model.parameters())[0])
             #print(list(model.parameters())[0].grad)        
-    torch.save(model.state_dic(),os.path.join('./logs/models','model_{}.pkl'.format(start_time)))
+        torch.save(model.state_dict(),os.path.join('./logs/models','model_{}_{}.pkl'.format(start_time,e_p)))
     log.close()
             
 if __name__=='__main__':
     ap=argparse.ArgumentParser() 
  
-    ap.add_argument('-e','--epoch',default=64)#Epoch
-    ap.add_argument('-b','--batch',default=16)#Batch_size
+    ap.add_argument('-e','--epoch',default=30)#Epoch
+    ap.add_argument('-b','--batch',default=32)#Batch_size
     ap.add_argument('-dv','--delta_v',default=.5)#delta_v
     ap.add_argument('-dd','--delta_d',default=3)#delta_d
     ap.add_argument('-l','--learning_rate',default=5e-4)#learning_rate
     ap.add_argument('-o','--optimizer',default='Adam')#optimizer
     ap.add_argument('-d','--device',default='GPU')#training device
     ap.add_argument('-t','--test_ratio',default=.1)
+    ap.add_argument('-s','--stage',default='new')
     #ap.add_argument('-cl','--class_weight',default=.5)
     #ap.add_argument()
     #ap.add_argument()
@@ -100,7 +103,14 @@ if __name__=='__main__':
     
     train_indices,test_indices=split_dataset(args['test_ratio'])
     data=build_sampler(TusimpleData('./data',transform=Rescale((256,512))),args['batch'],1,train_indices,test_indices)    
-    model=LaneNet()
+    
+    if args['stage']=='new':
+        model=LaneNet()
+    else:
+        model_file='model_1549814949_6.pkl'
+        model=LaneNet()
+        weight_dict=torch.load(os.path.join('./logs/models/',model_file))
+        model.load_state_dict(weight_dict.state_dict())
     
     train(model,data,args['epoch'],args['batch'],
           args['delta_v'],args['delta_d'],args['learning_rate'],
